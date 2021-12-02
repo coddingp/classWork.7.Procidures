2dbdfc748746385a082d69910af2da3bf830fa28
DotVR46 <=>
2021-12-02 19:51:59 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,54 @@
+//package com.company;
+//
+//import java.util.Scanner;
+//import java.util.Random;
+//public class Main {
+//    Random rnd=new Random((100+10)+1);
+//
+//    static int edd(int a, int b){
+//        return a+b;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] FirstLast = new int[]{1,2};
+//        System.out.println(edd(FirstLast[0],FirstLast[1]));
+//    }
+//}
+
+
+//package com.company;
+//
+//import java.util.Scanner;
+//public class Main {
+//    static void numbers(int a, int  b, int  c, int d){
+//        System.out.print(a + b + c + d);
+//    }
+//    public static void main(String[] args) {
+//        System.out.println("Enter a number: ");
+//        Scanner scn = new Scanner(System.in);
+//        int first1 = scn.nextInt();
+//        System.out.println("Enter second number: ");
+//        int second1 = scn.nextInt();
+//        System.out.println("Enter third number: ");
+//        int third1 = scn.nextInt();
+//        System.out.println("Enter fourth number: ");
+//        int fourth1 = scn.nextInt();
+//        System.out.println("The sum of your numbers is equal to: ");
+//        numbers(first1,second1,third1,fourth1);
+//    }
+//}
+//}
+//
+//package com.company;
+//
+//public class Main {
+//    static String a="bull ";
+//    public static  String b="bull";
+//
+//    public static void error(String x,String y){
+//        System.out.println(x+y);
+//    }
+//    public static void main(String[] args) {
+//	error(a, b);
+//    }
+//}
